39b48f3cb16f60e2328cd1105501e6360c973bb0
nikolaih <hansen.nikolai@gmail.com>
2022-05-15 14:35:24 +0200

    fixed cors error, will now allow options in router

--- api/routes.php
+++ api/routes.php
@@ -1,9 +1,8 @@
 <?php
 
-
 session_start();
-#ini_set('display_errors', 1);
-#ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 #error_reporting(E_ALL);
 
  // Allow from any origin
@@ -12,19 +11,20 @@ session_start();
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');    // cache for 1 day
     header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT'); 
-    header( 'Access-Control-Allow-Headers: Content-Type, x-requested-with, Accept, DNT, Referer, sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, User-Agent'); 
+    header( 'Access-Control-Allow-Headers: Authorization, Accept-Encoding, Accept-Language,Access-Control-Request-Headers, Origin, Referer,  Content-Type, x-requested-with, Accept, DNT, Referer, sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, Sec-Fetch-Dest, Sec-Fetch-Mode, Sec-Fetch-Site, User-Agent'); 
+ 
   }
-header( 'Access-Control-Allow-Headers: Authorization, Content-Type, x-requested-with, Accept, DNT, Referer, sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, User-Agent'); 
-
+header( 'Access-Control-Allow-Headers: Authorization, Accept-Encoding, Accept-Language,Access-Control-Request-Headers, Origin, Referer,  Content-Type, x-requested-with, Accept, DNT, Referer, sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, Sec-Fetch-Dest, Sec-Fetch-Mode, Sec-Fetch-Site, User-Agent'); 
+header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT'); 
 
 require_once realpath($_SERVER["DOCUMENT_ROOT"]) . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . 'FrontController.php';
 $frontController = new FrontController();
 
 /* The following section routes requests to the correct controller */
-$frontController->post(      '/api/meteors/load',       '/api/LoadController.php'              );  # Loading data from files
+$frontController->post(      '/api/meteors/load',       '/api/LoadController.php'               );  # Loading data from files
 $frontController->get(      '/api/meteors',             '/api/MeteorsController.php'            ); # Get all loaded meteors
 $frontController->get(      '/api/search/$query',       '/api/SearchController.php'             ); # Get all meteors by search string
-$frontController->get(      '/api/filter',              '/api/MeteorFilterContoroller.php'      ); # Get all meteors by filtering
+$frontController->get(      '/api/filter',              '/api/MeteorFilterController.php'       ); # Get all meteors by filtering
 $frontController->get(      '/api/meteor/$id',          '/api/MeteorController.php'             ); # Get single meteor
 $frontController->put(      '/api/meteor/$id',          '/api/MeteorController.php'             ); # Update meteor - e.g. update verification on meteor
 $frontController->post(     '/api/classify',            '/api/MeteorReviewController.php'       ); # Review meteor
@@ -32,8 +32,17 @@ $frontController->post(     '/api/user/new',            '/api/NewUserController.
 $frontController->post(     '/api/login',               '/api/LoginController.php'              ); # Login user
 $frontController->get(      '/api/users',               '/api/UsersController.php'              ); # Get all users
 $frontController->get(      '/api/user',                '/api/UserController.php'               ); # Get user
-$frontController->post(     '/api/user/myreviews',      '/api/MyReviewsController.php'         );  # Get users' reviews
+$frontController->post(     '/api/user/myreviews',      '/api/MyReviewsController.php'          );  # Get users' reviews
+$frontController->post(     '/api/user/myreviews',      '/api/MyReviewsController.php'          );  # Get users' reviews
 $frontController->get(      '/api/report/$report',      '/api/ReportController.php'             ); # Get report
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  header( 'Access-Control-Allow-Headers: Authorization, Accept-Encoding, Accept-Language,Access-Control-Request-Headers, Origin, Referer,  Content-Type, x-requested-with, Accept, DNT, Referer, sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, Sec-Fetch-Dest, Sec-Fetch-Mode, Sec-Fetch-Site, User-Agent'); 
+  header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT'); 
+  http_response_code(200);
+  die();
+}
+
 $frontController->any(      '/404',                     '/api/404.php'                          ); # 404
 
 
