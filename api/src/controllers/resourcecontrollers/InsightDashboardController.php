64cf8c2bd7cec0f8c35d1d139b0a586b98902f2b
nikolaih <hansen.nikolai@gmail.com>
2022-05-22 15:47:28 +0200

    Refactored report data extraction

--- api/src/controllers/resourcecontrollers/InsightDashboardController.php
+++ api/src/controllers/resourcecontrollers/InsightDashboardController.php
@@ -2,115 +2,15 @@
 
 require_once realpath($_SERVER["DOCUMENT_ROOT"]) . DIRECTORY_SEPARATOR . 'config.php';
 require_once realpath($_SERVER["DOCUMENT_ROOT"]) . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 'db.php';
+require_once realpath($_SERVER["DOCUMENT_ROOT"]).DIRECTORY_SEPARATOR.'api'.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'service'.DIRECTORY_SEPARATOR.'MeteorService.php'; 
+require_once realpath($_SERVER["DOCUMENT_ROOT"]) . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . 'resourcecontrollers' . DIRECTORY_SEPARATOR . 'AbstractController.php';
 
-
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET");
-
-
-require_once realpath($_SERVER["DOCUMENT_ROOT"]) . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . 'resourcecontrollers' . DIRECTORY_SEPARATOR . 'BaseController.php';
-
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST");
-
-class InsightDashboardController extends BaseController
+class InsightDashboardController extends AbstractController
 {
 	protected function get()
 	{
-
-		if ($this->resourceId == "cam") {
-			$sql = "select  CONCAT(UCASE(LEFT( s.station_name, 1)), 
-		SUBSTRING( s.station_name, 2)) as Stasjonsnavn, 
-c.cam_name as Kameranavn, 
-min(m.date) ForsteObservasjonsTidspunkt, 
-max(m.date) SisteObervasjonsTidspunkt, 
-count(distinct date(m.date)) DagerMedObservasjoner,
-DATEDIFF(now(),max(m.date)) as DagerSidenSisteObservasjon,
-count(*) as Kameraopptak, 
-count(distinct m.id) as Meteorer, 
-count(distinct case when m.track_startheight is not null then m.id end) Krysspeilede,
-COUNT(DISTINCT CASE WHEN m.track_startheight is not null and m.track_startheight < 40 THEN m.id END) Meteorittkandidater
-from station as s
-left outer join cam as c on s.id = c.station_id
-left outer join observation_cam_data as d on c.id = d.cam_id
-left outer join meteor as m on d.meteor_id = m.id
-group by   CONCAT(UCASE(LEFT( s.station_name, 1)), 
-		SUBSTRING( s.station_name, 2)), c.cam_name
-order by  CONCAT(UCASE(LEFT( s.station_name, 1)), 
-		SUBSTRING( s.station_name, 2)),c.cam_name";
-			$results = dbQuery($sql);
-			$rows = array();
-			while ($row = dbFetchAssoc($results)) {
-				$rows[] = $row;
-			}
-			echo json_encode($rows);
-		}
-
-		if ($this->resourceId == "station") {
-			$sql = "select 
-		CONCAT(UCASE(LEFT( s.station_name, 1)), 
-									SUBSTRING( s.station_name, 2)) as Stasjonsnavn,
-	   min(m.date) ForsteObservasjonsTidspunkt, 
-	   max(m.date) SisteObervasjonsTidspunkt, 
-	   count(distinct date(m.date)) DagerMedObservasjoner,
-	   DATEDIFF(now(),max(m.date)) as DagerSidenSisteObservasjon,
-	   count(*) as Kameraopptak, 
-	   count(distinct m.id) as Meteorer, 
-	   count(distinct case when m.track_startheight is not null then m.id end) Krysspeilede,
-	   COUNT(DISTINCT CASE WHEN m.track_startheight is not null and m.track_startheight < 40 THEN m.id END) Meteorittkandidater
-	   from station as s
-	   left outer join cam as c on s.id = c.station_id
-	   left outer join observation_cam_data as d on c.id = d.cam_id
-	   left outer join meteor as m on d.meteor_id = m.id
-	   group by   CONCAT(UCASE(LEFT( s.station_name, 1)), 
-									SUBSTRING( s.station_name, 2))
-	   order by  CONCAT(UCASE(LEFT( s.station_name, 1)), 
-									SUBSTRING( s.station_name, 2))";
-			$results = dbQuery($sql);
-			$rows = array();
-			while ($row = dbFetchAssoc($results)) {
-				$rows[] = $row;
-			}
-			echo json_encode($rows);
-		}
-
-
-		if ($this->resourceId == "total") {
-			$sql = "select  
-		min(m.date) ForsteObservasjonsTidspunkt, 
-		max(m.date) SisteObervasjonsTidspunkt, 
-		count(distinct date(m.date)) DagerMedObservasjoner,
-		DATEDIFF(now(),max(m.date)) as DagerSidenSisteObservasjon,
-		count(*) as Kameraopptak, 
-		count(distinct m.id) as Meteorer, 
-		count(distinct case when m.track_startheight is not null then m.id end) Krysspeilede,
-		COUNT(DISTINCT CASE WHEN m.track_startheight is not null and m.track_startheight < 40 THEN m.id END) Meteorittkandidater
-		from station as s
-		left outer join cam as c on s.id = c.station_id
-		left outer join observation_cam_data as d on c.id = d.cam_id
-		left outer join meteor as m on d.meteor_id = m.id";
-			$results = dbQuery($sql);
-			$rows = array();
-			while ($row = dbFetchAssoc($results)) {
-				$rows[] = $row;
-			}
-			echo json_encode($rows[0]);
-		}
-
-		if ($this->resourceId == "coordinates") {
-			$sql = "SELECT track_endlat lat, track_endlong lng FROM 153413_ildkule_dev.meteor where track_endlat is not null";
-			$results = dbQuery($sql);
-			$rows = array();
-			while ($row = dbFetchAssoc($results)) {
-				$rows[] = $row;
-			}
-			echo json_encode($rows);
-		}
-		;
-
-
+		$service = new MeteorService();
+		print ($service->getInsight($this->resourceId));
 	}
 
 
--- api/src/service/MeteorService.php
+++ api/src/service/MeteorService.php
@@ -12,8 +12,7 @@ require_once realpath($_SERVER["DOCUMENT_ROOT"]) . DIRECTORY_SEPARATOR . 'api' .
 require_once realpath($_SERVER["DOCUMENT_ROOT"]) . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'dao' . DIRECTORY_SEPARATOR . 'CamDao.php';
 require_once realpath($_SERVER["DOCUMENT_ROOT"]) . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'dao' . DIRECTORY_SEPARATOR . 'ObservationCamDataDao.php';
 require_once realpath($_SERVER["DOCUMENT_ROOT"]) . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'dao' . DIRECTORY_SEPARATOR . 'UserReviewDao.php';
-
-
+require_once realpath($_SERVER["DOCUMENT_ROOT"]) . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 'db.php';
 
 
 class MeteorService
@@ -57,9 +56,9 @@ class MeteorService
 
     public function getAllMeteors($page = -1, $limit = 10)
     {
-        $meteorDao = new MeteorDao();     
-        $meteors = $meteorDao->findAll($page,  $limit);
-        $count =  $meteorDao->getCount();
+        $meteorDao = new MeteorDao();
+        $meteors = $meteorDao->findAll($page, $limit);
+        $count = $meteorDao->getCount();
         $pages = ceil($count / $limit);
         $result = array("totalItems" => $count, "meteors" => $meteors, "totalPages" => $pages, "currentPage" => 1);
         return json_encode($result);
@@ -71,29 +70,30 @@ class MeteorService
         $meteor = $meteorDao->findByID($id);
 
         if ($meteor) {
-            
+
             $camDataDao = new ObservationCamDataDao();
             $meteorId = $meteor->id;
             $camDataFound = $camDataDao->findByMeteorID($meteorId);
             $reviewDao = new UserReviewDao();
             $reviewsFound = $reviewDao->findByMeteorID($meteorId);
-            $meteor->user_review =  $reviewsFound;
+            $meteor->user_review = $reviewsFound;
             $meteor->observation_cam_data = $camDataFound;
             $camDao = new CamDao();
             $stationDao = new StationDao();
-            $cams  = [];           
+            $cams = [];
             foreach ($meteor->observation_cam_data as $camData) {
                 $id = $camData->id;
                 $result = $camDao->findByCamDataID($id);
                 if ($result) {
                     array_push($cams, $result);
                     $camData->cam = $result;
-                    $camData->cam->station =  $stationDao->findByCamID($camData->cam->id);
+                    $camData->cam->station = $stationDao->findByCamID($camData->cam->id);
                 }
             }
             $result = $meteor;
             return json_encode($result);
-        } else {
+        }
+        else {
             return null;
         }
     }
@@ -102,7 +102,7 @@ class MeteorService
     {
         $meteorDao = new MeteorDao();
         $meteors = $meteorDao->search($searchString);
-        $result =  array("totalItems" => 800,  "meteors" => $meteors,  "totalPages" => 80, "currentPage" => 80);
+        $result = array("totalItems" => 800, "meteors" => $meteors, "totalPages" => 80, "currentPage" => 80);
         return json_encode($result);
     }
 
@@ -110,7 +110,105 @@ class MeteorService
     {
         $meteorDao = new MeteorDao();
         $meteors = $meteorDao->filter($stationName, $year, $meteorClass);
-        $result =  array("totalItems" => 800,  "meteors" => $meteors,  "totalPages" => 80, "currentPage" => 80);
+        $result = array("totalItems" => 800, "meteors" => $meteors, "totalPages" => 80, "currentPage" => 80);
         return json_encode($result);
     }
+
+    public function getInsight($reportName)
+    {
+        if ($reportName == "cam") {
+            $sql = "select  CONCAT(UCASE(LEFT( s.station_name, 1)), 
+		SUBSTRING( s.station_name, 2)) as Stasjonsnavn, 
+c.cam_name as Kameranavn, 
+min(m.date) ForsteObservasjonsTidspunkt, 
+max(m.date) SisteObervasjonsTidspunkt, 
+count(distinct date(m.date)) DagerMedObservasjoner,
+DATEDIFF(now(),max(m.date)) as DagerSidenSisteObservasjon,
+count(*) as Kameraopptak, 
+count(distinct m.id) as Meteorer, 
+count(distinct case when m.track_startheight is not null then m.id end) Krysspeilede,
+COUNT(DISTINCT CASE WHEN m.track_startheight is not null and m.track_startheight < 40 THEN m.id END) Meteorittkandidater
+from station as s
+left outer join cam as c on s.id = c.station_id
+left outer join observation_cam_data as d on c.id = d.cam_id
+left outer join meteor as m on d.meteor_id = m.id
+group by   CONCAT(UCASE(LEFT( s.station_name, 1)), 
+		SUBSTRING( s.station_name, 2)), c.cam_name
+order by  CONCAT(UCASE(LEFT( s.station_name, 1)), 
+		SUBSTRING( s.station_name, 2)),c.cam_name";
+            $results = dbQuery($sql);
+            $rows = array();
+            while ($row = dbFetchAssoc($results)) {
+                $rows[] = $row;
+            }
+            echo json_encode($rows);
+        }
+
+        if ($reportName == "station") {
+            $sql = "select 
+		CONCAT(UCASE(LEFT( s.station_name, 1)), 
+									SUBSTRING( s.station_name, 2)) as Stasjonsnavn,
+	   min(m.date) ForsteObservasjonsTidspunkt, 
+	   max(m.date) SisteObervasjonsTidspunkt, 
+	   count(distinct date(m.date)) DagerMedObservasjoner,
+	   DATEDIFF(now(),max(m.date)) as DagerSidenSisteObservasjon,
+	   count(*) as Kameraopptak, 
+	   count(distinct m.id) as Meteorer, 
+	   count(distinct case when m.track_startheight is not null then m.id end) Krysspeilede,
+	   COUNT(DISTINCT CASE WHEN m.track_startheight is not null and m.track_startheight < 40 THEN m.id END) Meteorittkandidater
+	   from station as s
+	   left outer join cam as c on s.id = c.station_id
+	   left outer join observation_cam_data as d on c.id = d.cam_id
+	   left outer join meteor as m on d.meteor_id = m.id
+	   group by   CONCAT(UCASE(LEFT( s.station_name, 1)), 
+									SUBSTRING( s.station_name, 2))
+	   order by  CONCAT(UCASE(LEFT( s.station_name, 1)), 
+									SUBSTRING( s.station_name, 2))";
+            $results = dbQuery($sql);
+            $rows = array();
+            while ($row = dbFetchAssoc($results)) {
+                $rows[] = $row;
+            }
+            echo json_encode($rows);
+        }
+
+
+        if ($reportName == "total") {
+            $sql = "select  
+		min(m.date) ForsteObservasjonsTidspunkt, 
+		max(m.date) SisteObervasjonsTidspunkt, 
+		count(distinct date(m.date)) DagerMedObservasjoner,
+		DATEDIFF(now(),max(m.date)) as DagerSidenSisteObservasjon,
+		count(*) as Kameraopptak, 
+		count(distinct m.id) as Meteorer, 
+		count(distinct case when m.track_startheight is not null then m.id end) Krysspeilede,
+		COUNT(DISTINCT CASE WHEN m.track_startheight is not null and m.track_startheight < 40 THEN m.id END) Meteorittkandidater
+		from station as s
+		left outer join cam as c on s.id = c.station_id
+		left outer join observation_cam_data as d on c.id = d.cam_id
+		left outer join meteor as m on d.meteor_id = m.id";
+            $results = dbQuery($sql);
+            $rows = array();
+            while ($row = dbFetchAssoc($results)) {
+                $rows[] = $row;
+            }
+            echo json_encode($rows[0]);
+        }
+
+        if ($reportName == "coordinates") {
+            $sql = "SELECT track_endlat lat, track_endlong lng FROM 153413_ildkule_dev.meteor where track_endlat is not null";
+            $results = dbQuery($sql);
+            $rows = array();
+            while ($row = dbFetchAssoc($results)) {
+                $rows[] = $row;
+            }
+            echo json_encode($rows);
+        }
+        ;
+
+
+
+
+    }
+
 }
