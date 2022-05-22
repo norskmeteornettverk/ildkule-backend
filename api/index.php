1a4b974fcd1b3d541d08dd8ef63a429970dbbede
nikolaih <hansen.nikolai@gmail.com>
2022-05-22 15:45:01 +0200

    Added bootstrap to clean up the index file

--- api/index.php
+++ api/index.php
@@ -1,24 +1,15 @@
 <?php
 
 require_once realpath($_SERVER["DOCUMENT_ROOT"]) . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'handlers' . DIRECTORY_SEPARATOR . 'ErrorHandler.php';
+require_once realpath($_SERVER["DOCUMENT_ROOT"]) . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  header( 'Access-Control-Allow-Headers: Authorization, Accept-Encoding, Accept-Language,Access-Control-Request-Headers, Origin, Referer,  Content-Type, x-requested-with, Accept, DNT, Referer, sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, Sec-Fetch-Dest, Sec-Fetch-Mode, Sec-Fetch-Site, User-Agent'); 
+  header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT'); 
+  http_response_code(200);
+  die();
+}
 
- // Allow from any origin
- if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT'); 
-    header( 'Access-Control-Allow-Headers: Authorization, Accept-Encoding, Accept-Language,Access-Control-Request-Headers, Origin, Referer,  Content-Type, x-requested-with, Accept, DNT, Referer, sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, Sec-Fetch-Dest, Sec-Fetch-Mode, Sec-Fetch-Site, User-Agent'); 
- 
-  }
-header( 'Access-Control-Allow-Headers: Authorization, Accept-Encoding, Accept-Language,Access-Control-Request-Headers, Origin, Referer,  Content-Type, x-requested-with, Accept, DNT, Referer, sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, Sec-Fetch-Dest, Sec-Fetch-Mode, Sec-Fetch-Site, User-Agent'); 
-header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT'); 
-
-require_once realpath($_SERVER["DOCUMENT_ROOT"]) . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . 'FrontController.php';
-require_once realpath($_SERVER["DOCUMENT_ROOT"]) . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'security' . DIRECTORY_SEPARATOR . 'Security.php';
 $frontController = new FrontController();
 
 /* The following section routes requests to the correct controller */
@@ -54,17 +45,10 @@ $frontController->post(	'/api/meteorload'	                      ,'/api/src/contr
 $frontController->post(	'/api/reportmeteor'	                    ,'/api/src/controllers/resourcecontrollers/ReportMeteorController.php'          ); # Post a seen meteor
 $frontController->post(	'/api/contact'	                        ,'/api/src/controllers/resourcecontrollers/ContactController.php'               ); # Post a contact form
 
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-  header( 'Access-Control-Allow-Headers: Authorization, Accept-Encoding, Accept-Language,Access-Control-Request-Headers, Origin, Referer,  Content-Type, x-requested-with, Accept, DNT, Referer, sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, Sec-Fetch-Dest, Sec-Fetch-Mode, Sec-Fetch-Site, User-Agent'); 
-  header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT'); 
-  http_response_code(200);
-  die();
-}
-
+// return 404 if the routing has not picked up the request
 http_response_code(404);
+echo json_encode(array('error' => "Resource or method doesn't exists", 'message' => 'Ressursen eller funksjonen eksisterer ikke'));
 
-//$frontController->any(      '/404',                     '/api/404.php'                          ); # 404
 
 /*
 // Dynamic GET. Example with 1 variable
