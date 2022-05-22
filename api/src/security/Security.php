3cb8ce9c38225ebb7c70cd35315848603fd8a179
nikolaih <hansen.nikolai@gmail.com>
2022-05-22 12:00:58 +0200

    Token handling functions

--- api/src/security/Security.php
+++ api/src/security/Security.php
@@ -43,12 +43,24 @@ function is_jwt_valid($jwt, $secret = 'secret') {
 function getUserFromToken($jwt) {	
 	$tokenParts = explode('.', $jwt);
 	$payload = base64_decode($tokenParts[1]);	
-	$token_header_array = json_decode($payload, true);
-	
+	$token_header_array = json_decode($payload, true);	
 	return  $token_header_array;
 	
 }
 
+function getLevelFromToken($jwt) {	
+	$tokenParts = explode('.', $jwt);
+	$payload = base64_decode($tokenParts[4]);	
+	$token_header_array = json_decode($payload, true);	
+	return  $token_header_array;	
+}
+
+function getRolesFromToken($jwt) {	
+	$tokenParts = explode('.', $jwt);
+	$payload = base64_decode($tokenParts[5]);	
+	$token_header_array = json_decode($payload, true);	
+	return  $token_header_array;	
+}
 function base64url_encode($data) {
     return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
 }
